b2a0512fc2a391f3ba4cb424fbd58dd7889536a7
shariktlt <shariktlt@gmail.com>
2019-10-25 02:23:45 +0300

    Каждый запуск логируем

--- core/src/main/java/solver/Main.java
+++ core/src/main/java/solver/Main.java
@@ -15,7 +15,7 @@ public class Main {
         String initUrl = "http://localhost:8080/race";
         //initUrl = "http://172.30.9.50:8080/race";
         String teamName = "Лоцманы";
-        teamName = "Отладка";
+        //teamName = "Отладка";
         Api api = new ExternalApi(teamName, initUrl);
 
         Solver solver = new Solver(api, new ExternalGraphBuilder(), new RouterPathFinder());
